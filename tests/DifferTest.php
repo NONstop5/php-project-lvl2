7427e58cf97b2588705fdf44ababb5b2b7e4ecc1
NONstop <non_2002@mail.ru>
2022-01-31 09:12:48 +0300

    fix DifferTest.php to add test cases

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -10,12 +10,53 @@ use function Differ\Differ\genDiff;
 
 class DifferTest extends TestCase
 {
-    public function testDiff()
+    private string $fixturesPath;
+
+    protected function setUp(): void
     {
-        $fixturesPath = __DIR__ . '/fixtures';
-        $pathToFile1 = "{$fixturesPath}/file1.json";
-        $pathToFile2 = "{$fixturesPath}/file2.json";
-        $expectedData = file_get_contents("{$fixturesPath}/expected.txt");
+        parent::setUp();
+
+        $this->fixturesPath = __DIR__ . '/fixtures';
+    }
+
+    public function testDiffJson()
+    {
+        $pathToFile1 = "{$this->fixturesPath}/file1.json";
+        $pathToFile2 = "{$this->fixturesPath}/file2.json";
+        $expectedData = file_get_contents("{$this->fixturesPath}/expected.txt");
+
+        $diff = genDiff($pathToFile1, $pathToFile2);
+
+        $this->assertEquals($expectedData, $diff);
+    }
+
+    public function testDiffYml()
+    {
+        $pathToFile1 = "{$this->fixturesPath}/file1.yml";
+        $pathToFile2 = "{$this->fixturesPath}/file2.yaml";
+        $expectedData = file_get_contents("{$this->fixturesPath}/expected.txt");
+
+        $diff = genDiff($pathToFile1, $pathToFile2);
+
+        $this->assertEquals($expectedData, $diff);
+    }
+
+    public function testDiffJsonYml()
+    {
+        $pathToFile1 = "{$this->fixturesPath}/file1.json";
+        $pathToFile2 = "{$this->fixturesPath}/file2.yaml";
+        $expectedData = file_get_contents("{$this->fixturesPath}/expected.txt");
+
+        $diff = genDiff($pathToFile1, $pathToFile2);
+
+        $this->assertEquals($expectedData, $diff);
+    }
+
+    public function testDiffYmlJson()
+    {
+        $pathToFile1 = "{$this->fixturesPath}/file1.yml";
+        $pathToFile2 = "{$this->fixturesPath}/file2.json";
+        $expectedData = file_get_contents("{$this->fixturesPath}/expected.txt");
 
         $diff = genDiff($pathToFile1, $pathToFile2);
 
