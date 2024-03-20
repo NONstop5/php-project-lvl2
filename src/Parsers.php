b267f96ac5b829a6cf9aee758dbe1306ea711c75
IgorIlyaguev <non_2002@mail.ru>
2024-03-20 14:05:07 +0300

    fix Parsers.php to create universal parse function

--- src/Parsers.php
+++ src/Parsers.php
@@ -4,7 +4,31 @@ declare(strict_types=1);
 
 namespace Differ\Parsers;
 
+use JsonException;
+use RuntimeException;
 use Symfony\Component\Yaml\Yaml;
+
+use const Differ\Differ\FILE_FORMAT_JSON;
+use const Differ\Differ\FILE_FORMAT_YAML;
+
+/**
+ * @param string $dataType
+ * @param string $data
+ *
+ * @return array
+ * @throws JsonException
+ */
+function parse(string $dataType, string $data): array
+{
+    switch ($dataType) {
+        case FILE_FORMAT_JSON:
+            return jsonFileParse($data);
+        case FILE_FORMAT_YAML:
+            return yamlFileParse($data);
+        default:
+            throw new RuntimeException('Unknown file format!');
+    }
+}
 
 /**
  * @param string $yamlFilePath
@@ -13,15 +37,16 @@ use Symfony\Component\Yaml\Yaml;
  */
 function yamlFileParse(string $yamlFilePath): array
 {
-    return Yaml::parseFile($yamlFilePath);
+    return Yaml::parse($yamlFilePath);
 }
 
 /**
- * @param string $jsonFilePath
+ * @param string $data
  *
  * @return array
+ * @throws JsonException
  */
-function jsonFileParse(string $jsonFilePath): array
+function jsonFileParse(string $data): array
 {
-    return json_decode(file_get_contents($jsonFilePath), true);
+    return json_decode($data, true, 512, JSON_THROW_ON_ERROR);
 }
