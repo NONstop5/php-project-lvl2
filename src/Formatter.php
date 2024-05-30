9b21813461ac2b6404cb39869e56b02e010ad33d
IgorIlyaguev <non_2002@mail.ru>
2024-05-31 00:54:04 +0300

    refactor Formatter.php

--- src/Formatter.php
+++ src/Formatter.php
@@ -4,13 +4,14 @@ declare(strict_types=1);
 
 namespace Differ\Formatter;
 
+use Differ\Formatters\Stylish;
 use RuntimeException;
 
 function format(string $format, array $data): string
 {
     switch ($format) {
         case 'stylish':
-            return \Differ\Formatters\Stylish\format($data);
+            return Stylish\format($data);
         default:
             throw new RuntimeException(sprintf('Unknown data format: "%s"!', $format));
     }
