69cf9a9ec60e68e25a393acf2e552d840480f6e7
IgorIlyaguev <non_2002@mail.ru>
2024-06-09 13:16:58 +0300

    fix Formatter.php to add plain format

--- src/Formatter.php
+++ src/Formatter.php
@@ -5,14 +5,17 @@ declare(strict_types=1);
 namespace Differ\Formatter;
 
 use Differ\Formatters\Stylish;
+use Differ\Formatters\Plain;
 use RuntimeException;
+
+const FORMAT_STYLISH = 'stylish';
+const FORMAT_PLAIN = 'plain';
 
 function format(string $format, array $data): string
 {
-    switch ($format) {
-        case 'stylish':
-            return Stylish\format($data);
-        default:
-            throw new RuntimeException(sprintf('Unknown data format: "%s"!', $format));
-    }
+    return match ($format) {
+        FORMAT_STYLISH => Stylish\format($data),
+        FORMAT_PLAIN => Plain\format($data),
+        default => throw new RuntimeException(sprintf('Unknown data format: "%s"!', $format)),
+    };
 }
