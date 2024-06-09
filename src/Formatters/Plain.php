e75e5ae69c2df17313b5c4e465dee9c246fd9869
IgorIlyaguev <non_2002@mail.ru>
2024-06-09 13:16:16 +0300

    add Plain.php

---
+++ src/Formatters/Plain.php
@@ -0,0 +1,96 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Differ\Formatters\Plain;
+
+use const Differ\Differ\ADDED;
+use const Differ\Differ\CHANGED;
+use const Differ\Differ\DELETED;
+use const Differ\Differ\NESTED;
+use const Differ\Differ\UNCHANGED;
+
+const COMPARE_TEXT_MAP = [
+    ADDED => 'added',
+    DELETED => 'removed',
+    CHANGED => 'updated',
+    UNCHANGED => '',
+    NESTED => '[complex value]',
+];
+
+function format(array $data): string
+{
+    return iter($data) . PHP_EOL;
+}
+
+function iter(mixed $value, array $acc = []): string
+{
+    if (!is_array($value)) {
+        return toString($value);
+    }
+
+    if (!array_key_exists(0, $value) && !array_key_exists('compare', $value)) {
+        return toString($value);
+    }
+
+    $result = array_map(
+        static function ($val) use ($acc) {
+            $key = $val['key'];
+            $compare = $val['compare'];
+            $compareText = getCompareText($compare);
+            $acc[] = $key;
+
+            return match ($compare) {
+                ADDED => sprintf(
+                    "Property '%s' was %s with value: %s\n",
+                    implode('.', $acc),
+                    $compareText,
+                    iter($val['value'], $acc),
+                ),
+                DELETED => sprintf(
+                    "Property '%s' was %s\n",
+                    implode('.', $acc),
+                    $compareText,
+                ),
+                CHANGED => sprintf(
+                    "Property '%s' was %s. From %s to %s\n",
+                    implode('.', $acc),
+                    $compareText,
+                    iter($val['value1'], $acc),
+                    iter($val['value2'], $acc),
+                ),
+                NESTED => iter($val['value'], $acc),
+                default => null,
+            };
+        },
+        $value
+    );
+
+    return implode($result);
+}
+
+function getCompareText(string $compareText): string
+{
+    return COMPARE_TEXT_MAP[$compareText];
+}
+
+function toString(mixed $value): string
+{
+    if (is_null($value)) {
+        return 'null';
+    }
+
+    if (is_bool($value)) {
+        return $value ? 'true' : 'false';
+    }
+
+    if (is_string($value)) {
+        return "'{$value}'";
+    }
+
+    if (is_array($value)) {
+        return '[complex value]';
+    }
+
+    return trim(var_export($value, true), "'");
+}
