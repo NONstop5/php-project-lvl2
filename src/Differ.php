155f45804e3aa7373210f8b491152a0e529d18b5
NONstop <non_2002@mail.ru>
2024-06-12 11:17:45 +0300

    refactor Differ.php

--- src/Differ.php
+++ src/Differ.php
@@ -54,62 +54,58 @@ function buildDiffData(array $data1, array $data2): array
 
 function buildDiffIter(array $data1, array $data2): array
 {
-    $result1 = array_map(
-        static function (string $key1, mixed $value1) use ($data2) {
-            if (array_key_exists($key1, $data2)) {
-                $value2 = $data2[$key1];
+    $uniqueKeys = array_unique(array_merge(array_keys($data1), array_keys($data2)));
+    $sortedKeys = sort($uniqueKeys, static function ($key1, $key2) {
+        return $key1 <=> $key2;
+    });
 
-                if (is_array($value1) && is_array($value2)) {
-                    return [
-                        'compare' => NESTED,
-                        'key' => $key1,
-                        'value' => buildDiffIter($value1, $value2),
-                    ];
-                }
+    return array_map(
+        static function (string $key) use ($data1, $data2) {
+            $value1 = $data1[$key] ?? null;
+            $value2 = $data2[$key] ?? null;
 
-                if ($value1 === $value2) {
-                    return [
-                        'compare' => UNCHANGED,
-                        'key' => $key1,
-                        'value' => $value1,
-                    ];
-                }
-
+            if (
+                (is_array($value1) && !array_is_list($value1)) &&
+                (is_array($value2) && !array_is_list($value2))
+            ) {
                 return [
-                    'compare' => CHANGED,
-                    'key' => $key1,
-                    'value1' => $value1,
-                    'value2' => $value2,
+                    'compare' => NESTED,
+                    'key' => $key,
+                    'value' => buildDiffIter($value1, $value2),
+                ];
+            }
+
+            if (!array_key_exists($key, $data1)) {
+                return [
+                    'compare' => ADDED,
+                    'key' => $key,
+                    'value' => $value2,
+                ];
+            }
+
+            if (!array_key_exists($key, $data2)) {
+                return [
+                    'compare' => DELETED,
+                    'key' => $key,
+                    'value' => $value1,
+                ];
+            }
+
+            if ($value1 === $value2) {
+                return [
+                    'compare' => UNCHANGED,
+                    'key' => $key,
+                    'value' => $value1,
                 ];
             }
 
             return [
-                'compare' => DELETED,
-                'key' => $key1,
-                'value' => $value1,
-            ];
-        },
-        array_keys($data1),
-        $data1
-    );
-
-    $json2Unique = array_diff_key($data2, $data1);
-
-    $result2 = array_map(
-        static function ($key, $value) {
-            return [
-                'compare' => ADDED,
+                'compare' => CHANGED,
                 'key' => $key,
-                'value' => $value,
+                'value1' => $value1,
+                'value2' => $value2,
             ];
         },
-        array_keys($json2Unique),
-        $json2Unique
+        $sortedKeys
     );
-
-    $result = [...$result1, ...$result2];
-
-    return sort($result, static function ($item1, $item2) {
-        return $item1['key'] <=> $item2['key'];
-    });
 }
